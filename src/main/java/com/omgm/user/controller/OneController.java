34f454a68a0bb154ee048cb41c70f9c3e2553bc4
nar <yoyo7227@naver.com>
2020-07-29 18:24:27 +0900

    review folder 구조 변경

--- src/main/java/com/omgm/user/controller/OneController.java
+++ src/main/java/com/omgm/user/controller/OneController.java
@@ -2,18 +2,11 @@ package com.omgm.user.controller;
 
 import com.omgm.user.board.BoardService;
 import com.omgm.user.board.BoardVO;
-import com.omgm.user.review.ReviewVO;
+import com.omgm.user.review.bean.ReviewVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class OneController {
