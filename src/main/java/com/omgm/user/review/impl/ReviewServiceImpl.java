77cc7247a0a36fdf27a7fafb433edbe4d935a49f
YongSunJang <7117wkd@naver.com>
2020-07-30 20:00:27 +0900

    페이지네이션 구현중

--- src/main/java/com/omgm/user/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/omgm/user/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,6 @@
 package com.omgm.user.review.impl;
 
+import com.omgm.user.review.beans.PageNavigator;
 import com.omgm.user.review.beans.ReviewDAO;
 import com.omgm.user.review.beans.ReviewVO;
 import com.omgm.user.review.service.ReviewService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service("reviewService")
 public class ReviewServiceImpl implements ReviewService {
@@ -18,4 +20,5 @@ public class ReviewServiceImpl implements ReviewService {
     public List<ReviewVO> getReviewList(ReviewVO vo) {
         return reviewDAO.getReviewList(vo);
     }
+
 }
--- src/main/java/com/omgm/user/review/service/ReviewService.java
+++ src/main/java/com/omgm/user/review/service/ReviewService.java
@@ -1,9 +1,11 @@
 package com.omgm.user.review.service;
 
+import com.omgm.user.review.beans.PageNavigator;
 import com.omgm.user.review.beans.ReviewVO;
 
 import java.util.List;
 
 public interface ReviewService {
     List<ReviewVO> getReviewList(ReviewVO vo);
+
 }
