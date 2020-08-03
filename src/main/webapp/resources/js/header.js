1f15785750da9a3440e74ad974f733d07d0f1852
nar <yoyo7227@naver.com>
2020-08-03 13:37:55 +0900

    구글 회원가입창 조정완료

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -154,5 +154,90 @@ function signGoogle() {
         xhr.open('GET', 'google.lo',true);
         xhr.setRequestHeader('Content-type', 'application/json');
         xhr.send(null);
+}``
+
+function snsSign() {
+    document.querySelector('signUp div div').innerHTML =
+        `<label for="sns_popup"></label>
+                <form action="#" class="sns_signup-form" onsubmit="return snsSignCheck(event)">
+                    <h1 style="font-weight: 700;">Google 회원가입</h1>
+                    <div class="txtboxID">
+                       <input name="id" type="text" required autocomplete=off>
+                       <span data-placeholder="ID"></span>
+                   </div>
+                   <input type="button" class="checkbtn" value="check" onclick="idCheck(false)">
+                    <div class="txtboxTEL1">
+                        <input name="tel1"  type="tel" pattern="0[0-9]{2}" required autocomplete=off>
+                        <span data-placeholder="Tel"></span><a>ㅡ</a>
+                    </div>
+                    <div class="txtboxTEL2">
+                        <input name="tel2"  type="tel" pattern="[0-9]{4}" required autocomplete=off><a>ㅡ</a>
+                        <span data-placeholder="Tel2"></span>
+                    </div>
+                    <div class="txtboxTEL3">
+                        <input name="tel3"  type="tel" pattern="[0-9]{4}" required autocomplete=off>
+                        <span data-placeholder="Tel3"></span>
+                    </div>
+                    <div class="txt_zipCode">
+                        <input id="sign_zipCode" name="zipCode"  type="number" required autocomplete=off>
+                        <span data-placeholder="ZipCode"></span>
+                    </div>
+                    <input type="button" class="searchbtn" value="search" onclick="zipCheck()">
+                    <div class="txt">
+                        <input id="sign_address"  name="address"  type="text" required autocomplete=off>
+                        <span data-placeholder="Address"></span>
+                    </div>
+                    <input type="submit" class="signup" value="Sign UP">
+               </form>
+                `;
+    document.querySelector('#popup').checked = true;
+    $(".txt input, .txtboxID input, .txtboxTEL1 input, .txtbox input, .txt_zipCode input").on("focus",function(){
+        $(this).addClass("focus");
+    });
+
+    $(".txt input, .txtboxID input, .txtboxTEL1 input, .txtbox input, .txt_zipCode input").on("blur",function(){
+        if($(this).val() === "") {
+            $(this).removeClass("focus");
+        }
+    });
+}
+
+function snsSignCheck(event) {
+    if(idCheck(true)) return false;
+
+    event.preventDefault();
+
+    const form = document.querySelector('.sns_signup-form');
+    const phone = form.tel1.value + '-' + form.tel2.value + '-' +  form.tel3.value;
+    const data = {
+        id: form.id.value,
+        pwd: form.pwd.value,
+        name: form.name.value,
+        phone: phone,
+        email: form.email.value,
+        zipcode: form.zipCode.value,
+        address: form.address.value
+    };
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const object = JSON.parse(xhr.responseText);
+            console.log(object.id);
+            document.querySelector('signUp div div').innerHTML =
+                `<label for="popup"></label>
+            <div style="background-color: #F28888; height: 300px; color: white; text-align: center;">
+                <img style="width:300px; margin: 30px auto;" src="resources/img/자산%208.png" alt="">
+                <h1 style="margin: 30px;">저희 오묘가묘의 가족이 되신 것을 진심으로 환영합니다!</h1>
+                <h1 style="margin: 30px;">로그인 하셔서 반려동물을 위한 다양한 서비스를 경험하세요!</h1>
+            </div>
+        `;
+        }
+    }
+
+    xhr.open('POST', 'addMember.lo',true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(data));
 }
 
