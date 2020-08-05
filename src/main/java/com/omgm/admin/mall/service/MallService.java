e53929aecf9d1f61493588207ecc63619f52b75e
YongSunJang <7117wkd@naver.com>
2020-08-05 09:48:58 +0900

    사료 등록 업데이트 구현중 및 주석 달았음

--- src/main/java/com/omgm/admin/mall/service/MallService.java
+++ src/main/java/com/omgm/admin/mall/service/MallService.java
@@ -10,21 +10,22 @@ import java.util.List;
 public interface MallService {
 
     // 토이 관련 서비스
-    void deleteMallToy(MallToyVO vo) throws Exception;
-    void insertMallToy(MallToyVO vo) throws Exception;
-    void updateMallToy(MallToyVO vo) throws Exception;
-    List<MallToyVO> getMallToyList() throws Exception;
+    void deleteMallToy(MallToyVO vo) throws Exception; // 장난감 등록 해제
+    void insertMallToy(MallToyVO vo) throws Exception; // 장난감 등록
+    void updateMallToy(MallToyVO vo) throws Exception; // 장난감 정보 수정
+    List<MallToyVO> getMallToyList() throws Exception; // 장난감 목록 나열
+    MallToyVO getMallToyOne(MallToyVO vo) throws Exception; // 장난감 상세정보 페이지
 
-    //상품관련 정보 페이지 불러오기
-    MallToyVO getMallToyOne(MallToyVO vo) throws Exception;
-    MallFeedVO getMallFeedOne(MallFeedVO vo) throws Exception;
-    void insertMallFeed(MallFeedVO vo) throws Exception;
-
+    // 사료 관련 서비스
+    void insertMallFeed(MallFeedVO vo) throws Exception; // 사료 등록
+    void updateMallFeed(MallFeedVO vo) throws Exception; // 사료 정보 수정
+    List<MallFeedVO> getMallFeedList() throws Exception; // 사료 목록 나열
+    MallFeedVO getMallFeedOne(MallFeedVO vo) throws Exception; // 사료 상세정보 페이지
 
     //관리자 주문목록페이지
     void insertMallOrder(MallOrderVO vo) throws Exception;
     List<MallOrderVO> getMallOrderList() throws Exception;
     void deleteMallOrder(MallOrderVO vo) throws Exception;
-    List<MallFeedVO> getMallFeedList() throws Exception;
+
 
 }
