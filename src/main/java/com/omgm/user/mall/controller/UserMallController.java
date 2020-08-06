159621a6d59958cae0c0ec4810a6eb7d3527b480
YongSunJang <7117wkd@naver.com>
2020-08-06 15:14:58 +0900

    클라이언트 상품 선택 관련 기능 구현중

--- src/main/java/com/omgm/user/mall/controller/UserMallController.java
+++ src/main/java/com/omgm/user/mall/controller/UserMallController.java
@@ -1,24 +1,31 @@
 package com.omgm.user.mall.controller;
 
+import com.omgm.user.mall.beans.PageNavigatorMall;
 import com.omgm.user.mall.beans.UserMallFeedVO;
 import com.omgm.user.mall.service.UserMallService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserMallController {
-    @Autowired
-    UserMallService UserMallService;
 
-    //MallList
+    @Autowired
+    UserMallService userMallService;
+
+    ////////////////////////// 사료 관련 컨트롤러 /////////////////////////
+    //MallList(+ 페이징 처리 추가)
     @RequestMapping("/getMallFeedList.do")
-    public ModelAndView getMallFeedList(UserMallFeedVO vo) throws Exception{
+    public ModelAndView getMallFeedList(UserMallFeedVO vo, @RequestParam(value="page", defaultValue = "1") int page) throws Exception{
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("/sales/productList");
-        mav.addObject("FeedList", UserMallService.getMallFeedList());
-        mav.addObject("ToyList", UserMallService.getMallToyList());
+        int COUNTERPAGE = 8;
+        int PAGEPERGROUP = 5;
+        PageNavigatorMall navi = new PageNavigatorMall(COUNTERPAGE, PAGEPERGROUP, page, userMallService.selectCountMall());
+        mav.setViewName("/sales/productFeedList");
+        mav.addObject("FeedList", userMallService.getMallFeedList(vo, navi));
+        mav.addObject("navi", navi);
         return mav;
     }
 
@@ -27,10 +34,10 @@ public class UserMallController {
     public ModelAndView productClick(UserMallFeedVO vo) throws Exception{
         ModelAndView mav = new ModelAndView();
         mav.setViewName("redirect:/toy.do");
-
         return mav;
     }
 
+    ////////////////////////// 장난감 관련 컨트롤러 /////////////////////////
 }
 
 
