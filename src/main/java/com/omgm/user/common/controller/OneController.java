f232ef1e26a74758d09aecb4fe61f14011f2990e
nar <yoyo7227@naver.com>
2020-08-08 16:37:27 +0900

    유치원 달력 클라이언트 부분 불러오기 디비 작업

--- src/main/java/com/omgm/user/common/controller/OneController.java
+++ src/main/java/com/omgm/user/common/controller/OneController.java
@@ -40,10 +40,8 @@ public class OneController {
     public ModelAndView kinderGarden(KinderGardenInfoVO vo) {
         ModelAndView mav = new ModelAndView();
         vo = commonService.getSchedule();
-        System.out.println(vo);
         List<KinderGardenInfoRowVO> list = commonService.getScheduleRow(vo);
         mav.addObject("day",vo);
-        System.out.println(list);
         mav.addObject("dayRow",list);
         mav.setViewName("/omgmInfo/kinderGarden");
         return mav;
