782a074d8b2a67009c622e390d196b1f7466d453
nar <yoyo7227@naver.com>
2020-08-10 19:34:18 +0900

    유치원 결제 디비 저장 작업 완료

--- src/main/java/com/omgm/user/common/beans/KinderGardenReservationVO.java
+++ src/main/java/com/omgm/user/common/beans/KinderGardenReservationVO.java
@@ -7,7 +7,7 @@ public class KinderGardenReservationVO {
     private int price;
     private String buyerName;
     private String animal;
-    private int animal_age;
+    private int animalAge;
     private String phone;
     private String email;
     private String zipcode;
@@ -18,14 +18,14 @@ public class KinderGardenReservationVO {
 
     public KinderGardenReservationVO() { }
 
-    public KinderGardenReservationVO(int seq, String name, String service, int price, String buyerName, String animal, int animal_age, String phone, String email, String zipcode, String address, String etc, String merchantUid, String pgProvider) {
+    public KinderGardenReservationVO(int seq, String name, String service, int price, String buyerName, String animal, int animalAge, String phone, String email, String zipcode, String address, String etc, String merchantUid, String pgProvider) {
         this.seq = seq;
         this.name = name;
         this.service = service;
         this.price = price;
         this.buyerName = buyerName;
         this.animal = animal;
-        this.animal_age = animal_age;
+        this.animalAge = animalAge;
         this.phone = phone;
         this.email = email;
         this.zipcode = zipcode;
@@ -83,12 +83,12 @@ public class KinderGardenReservationVO {
         this.animal = animal;
     }
 
-    public int getAnimal_age() {
-        return animal_age;
+    public int getAnimalAge() {
+        return animalAge;
     }
 
-    public void setAnimal_age(int animal_age) {
-        this.animal_age = animal_age;
+    public void setAnimalAge(int animalAge) {
+        this.animalAge = animalAge;
     }
 
     public String getPhone() {
@@ -156,7 +156,7 @@ public class KinderGardenReservationVO {
                 ", price=" + price +
                 ", buyerName='" + buyerName + '\'' +
                 ", animal='" + animal + '\'' +
-                ", animal_age=" + animal_age +
+                ", animalAge=" + animalAge +
                 ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
                 ", zipcode='" + zipcode + '\'' +
