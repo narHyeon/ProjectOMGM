ab8700e63c59c3cdd9582bddf33e2fc9f5c46fff
wnfl2215 <yujury76@naver.com>
2020-08-11 16:29:42 +0900

    충돌 수정

--- src/main/java/com/omgm/admin/mall/controller/MallController.java
+++ src/main/java/com/omgm/admin/mall/controller/MallController.java
@@ -6,6 +6,7 @@ import com.omgm.member.beans.MemberVO;
 import com.omgm.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
