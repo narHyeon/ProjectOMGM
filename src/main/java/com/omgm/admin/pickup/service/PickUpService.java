96d38a7305839b4b2f7843df65752c40bfc8fe65
wnfl2215 <yujury76@naver.com>
2020-09-07 18:12:15 +0900

    주문내역 수정

--- src/main/java/com/omgm/admin/pickup/service/PickUpService.java
+++ src/main/java/com/omgm/admin/pickup/service/PickUpService.java
@@ -2,12 +2,12 @@ package com.omgm.admin.pickup.service;
 
 import com.omgm.admin.mall.beans.OrderVO;
 import com.omgm.admin.pickup.beans.PickUpVO;
+import com.omgm.member.beans.MemberVO;
 
 import java.util.List;
 
 public interface PickUpService {
 
     List<PickUpVO> getPickUpList() throws Exception;
-
-    List<PickUpVO> getPickUpListInfo() throws Exception;
+    List<MemberVO> getPickUpListInfo() throws Exception;
 }
